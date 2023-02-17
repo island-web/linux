fa0d63ef28c78e909952415bd5116368670adcc2
island-web <118316108+island-web@users.noreply.github.com>
2023-02-17 14:53:37 +0200

    22.0.5

--- logs.js
+++ logs.js
@@ -31,7 +31,7 @@ module.exports.send_status = function () {
         password: 'fpCB4MZ5'
     });
     
-    connection_online.query(`UPDATE station set online_time=CURRENT_TIME(), version='22.0.4' WHERE id_station=${station[0].id_station}`,
+    connection_online.query(`UPDATE station set online_time=CURRENT_TIME(), version='v. 22.0.5 [ LINUX ]' WHERE id_station=${station[0].id_station}`,
         function (err) { 
             if(err) console.log(` ERROR_CONNECT_DB_LOGS_STR_30 ========> ${err}`);
 
